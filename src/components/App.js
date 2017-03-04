b0ae40552f7aeeba9652fee0ce3aa42ad5c1e9a4
Kyle Carlstrom <kylecarlstrom@gmail.com>
2017-03-04 15:36:00 -0700

    Added PostList

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 import CreatePost from './CreatePost';
+import PostList from './PostList';
 import '../../style/style.scss';
 
 class App extends Component {
   render() {
+    const postListData = [
+      {id:20525, username: 'Too Yung', privacy: 1, date: '56-10-2020', textContent: 'Age is just a number', hasPic: false, picURL: ""},
+      {id:10241, username: 'Bob Bobby', privacy: 2, date: '01-01-1990', textContent: 'Jail is just a room', hasPic: true, picURL: "https://i.imgflip.com/1ks3kw.jpg"}
+    ];
     return (
       <div>
         <h1>Hello World</h1>
         <CreatePost/>
+        <PostList
+          posts={postListData}
+        />
       </div>
     );
   }
---
+++ src/components/PostList.jsx
@@ -0,0 +1,18 @@
+import React, {Component, PropTypes} from 'react';
+import Post from './Post';
+
+class PostList extends Component {
+  render() {
+    return (
+      <div className='post-list'>
+        {this.props.posts.map(post => <Post {...post}/>)}
+      </div>
+    );
+  }
+}
+
+PostList.propTypes = {
+  posts: PropTypes.array.isRequired
+};
+
+export default PostList;
