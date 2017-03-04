0bddc552f3808f38e2dcbe0c1a0a47e9b23106af
Kyle Carlstrom <kylecarlstrom@gmail.com>
2017-03-04 15:02:27 -0700

    Added post component

---
+++ src/components/Post.jsx
@@ -0,0 +1,60 @@
+import React, {Component, PropTypes} from 'react';
+import {Panel, Button, FormControl} from 'react-bootstrap';
+
+class Post extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      commentText: ''
+    };
+
+    this.handleAddComment = this.handleAddComment.bind(this);
+    this.handleChangeComment = this.handleChangeComment.bind(this);
+  }
+
+  handleAddComment() {
+    // Add a comment
+  }
+
+  handleChangeComment(event) {
+    this.setState({
+      commentText: event.target.value
+    });
+  }
+
+  render() {
+    return (
+      <div className='post'>
+        <Panel>
+            <div className='poster-header'>
+                {this.props.username}
+            </div>
+            <div className='post-body'>
+                {this.props.postText}
+            </div>
+            <div className='post-footer'>
+                {/* Add comment list here */}
+                <FormControl
+                  type="text"
+                  value={this.state.commentText}
+                  placeholder="Add a comment"
+                  onChange={this.handleChangeComment}
+                />
+                <Button
+                  onClick={this.handleAddComment}>
+                  Add Comment
+                </Button>
+            </div>
+        </Panel>
+      </div>
+    );
+  }
+}
+
+Post.propTypes = {
+  postText: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired
+};
+
+export default Post;
