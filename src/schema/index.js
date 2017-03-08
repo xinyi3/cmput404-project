83c49b296aa414c0d5f5afc9faa0a893be55c49b
Kyle Carlstrom <kylecarlstrom@gmail.com>
2017-03-08 12:37:07 -0700

    Fixed citation

--- src/schema/index.js
+++ src/schema/index.js
@@ -1,6 +1,6 @@
 import {schema} from 'normalizr';
 
-// https://github.com/paularmstrong/normalizr/blob/master/docs/quickstart.md Paula Armstrong (MIT)
+// https://github.com/paularmstrong/normalizr/blob/master/docs/quickstart.md Paula Armstrong (https://github.com/paularmstrong) (MIT)
 const user = new schema.Entity('users');
 const comment = new schema.Entity('comments', {
   author: user
