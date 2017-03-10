a5b06caf8c758f27fe3eb58856c22586edbefe19
Kyle Carlstrom <kylecarlstrom@gmail.com>
2017-03-10 14:52:43 -0700

    Fixed some issues with field names

--- server/quickstart/models.py
+++ server/quickstart/models.py
@@ -5,10 +5,10 @@ from django.db import models
 # Create your models here.
 
 class Author(models.Model):
-    name = models.CharField(max_length=100)
+    displayName = models.CharField(max_length=100)
 
     def __unicode__(self):
-        return self.name
+        return self.displayName
 
 class Post(models.Model):
     text = models.CharField(max_length=140)
@@ -21,10 +21,10 @@ class Post(models.Model):
 class Comment(models.Model):
     post = models.ForeignKey(Post, related_name='comments')
     author = models.ForeignKey(Author)
-    text = models.CharField(max_length=140)
+    comment = models.CharField(max_length=140)
 
     def __unicode__(self):
-        return self.text
+        return self.comment
 
 class FollowingRelationship(models.Model):
     # Written by http://stackoverflow.com/a/13496120 user1839132 (http://stackoverflow.com/users/1839132/user1839132),
