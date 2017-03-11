4d853d865fb62e8e8491dcbfdee18d410e4e692a
Tian Zhi Wang <wtianzhi@gmail.com>
2017-03-10 17:50:33 -0700

    Deploy

--- server/settings.py
+++ server/settings.py
@@ -129,3 +129,7 @@ USE_TZ = True
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(PROJECT_ROOT, "static")
+]
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
